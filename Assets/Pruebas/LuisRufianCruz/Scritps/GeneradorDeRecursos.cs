68632092c7116ccae5e64aceb4c35c6cd0b5b98f
Luis Rufian Cruz <lrufian98@gmail.com>
2019-01-29 19:40:17 +0100

    #3 Generador Electricidad
    Codigo Comentado

--- Assets/Pruebas/LuisRufianCruz/Scritps/GeneradorDeRecursos.cs
+++ Assets/Pruebas/LuisRufianCruz/Scritps/GeneradorDeRecursos.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class GeneradorDeRecursos : MonoBehaviour
 {
-    bool dentroElectricidad = false;
-    bool produciendo = false;
-    public float tiempo = 1;
+    bool dentroElectricidad = false;  //Bool para determinar si el personaje se encuentra en la habitación de electricidad
+    bool produciendo = false;   //Bool para determinar si el personaje está produciendo algún recurso
+    public float tiempo = 1;    // Float para saber el tiempo que tardan los recursos en generarse
     
 
     // Start is called before the first frame update
@@ -19,9 +19,9 @@ public class GeneradorDeRecursos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (dentroElectricidad)
+        if (dentroElectricidad)         //Condicional dentro de update para que se sepa siempre si el personaje está dentro de la habitación de electricidad
         {
-            if (!produciendo)
+            if (!produciendo)           //Si el personaje no está produciendo ningún recurso, activa el bool "produciendo" y comienza la corutina que produce electricidad
             {
                 produciendo = true;
                 StartCoroutine(sumaElectricidad());
@@ -29,7 +29,7 @@ public class GeneradorDeRecursos : MonoBehaviour
 
             }
         }
-        else
+        else                            // Si no está dentro de la habitación, se detienen todas la corutinas que se estén ejecutando en este personaje, y desactiva el bool que indica que se están produciendo recursos
         {
             StopAllCoroutines();
             produciendo = false;
@@ -38,7 +38,7 @@ public class GeneradorDeRecursos : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("HabitacionElectricidad"))
+        if (col.gameObject.CompareTag("HabitacionElectricidad"))        //Cuando el personaje entra y está en la habitación de electricidad activa un bool que indica que está dentro de la habitación de electricidad
         {
 
             dentroElectricidad = true;
@@ -46,17 +46,17 @@ public class GeneradorDeRecursos : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D col)             
     {
-        if (col.gameObject.CompareTag("HabitacionElectricidad"))
+        if (col.gameObject.CompareTag("HabitacionElectricidad"))        //Cuando el personaje sale de la habitación de electricidad desactiva un bool que indica que está dentro de la habitación de electricidad
         {
-
+                                                                    
             dentroElectricidad = false;
 
         }
     }
 
-    IEnumerator sumaElectricidad()
+    IEnumerator sumaElectricidad()                                      //Corutina para generar el recurso de electricidad mientras la cantidad de electricidad almacenada sea menor a la capacidad de almacenaje de electricidad
     {
         while (produciendo) { 
             yield return new WaitForSeconds(tiempo);
