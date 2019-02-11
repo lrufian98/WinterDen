43a7e74fec531d7d0ca642a215602d9bce853433
Reyseta <luis.arg.gar@gmail.com>
2019-02-11 20:49:11 +0100

    #77 ha costado pero esta lista, todo por el team

---
+++ Assets/Pruebas/LuisArgandoñaGarzón/script/MenuCRTL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuCRTL : MonoBehaviour
+{
+   public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+   
+        
+    }
