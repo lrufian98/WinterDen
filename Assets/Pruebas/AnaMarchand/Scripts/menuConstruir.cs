f1daa7967dd720a3e1e1950971e8bc939ad9571c
AnaMarchand <ana.marchand@hotmail.com>
2019-02-11 20:54:54 +0100

    ignorar

---
+++ Assets/Pruebas/AnaMarchand/Scripts/menuConstruir.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class menuConstruir : MonoBehaviour, IPointerDownHandler
+{
+
+    public GameObject menuConstruccion;
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            menuConstruccion.SetActive(true);
+        }
+        else
+        {
+            menuConstruccion.SetActive(false);
+        }
+    }
+}
