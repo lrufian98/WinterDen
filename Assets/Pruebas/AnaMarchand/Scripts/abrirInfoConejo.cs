1d8b63feed2967ca95c579684148c26754e3b9b1
ana.marchand <ana.marchand@hotmail.com>
2019-02-11 13:29:05 +0100

    #84 Crear Pantalla de Informacion Conejo

---
+++ Assets/Pruebas/AnaMarchand/Scripts/abrirInfoConejo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class abrirInfoConejo : MonoBehaviour, IPointerDownHandler
+{
+
+    public GameObject CanvasInfoConejo;
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            CanvasInfoConejo.SetActive(true);
+        }
+        else
+        {
+            CanvasInfoConejo.SetActive(false);
+        }
+    }
+}
