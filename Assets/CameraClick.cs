9cf8c27178b2074de6dd1711d33d4d9740473e08
AnaMarchand <ana.marchand@hotmail.com>
2019-02-13 21:02:52 +0100

    Intento de Generador de Enemigos

--- Assets/CameraClick.cs
+++ Assets/CameraClick.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class CameraClick : MonoBehaviour
 {
-    Vector3 posicionInicial;
+    //Vector3 posicionInicial;
     // Start is called before the first frame update
-    void Start()
-    {
-        posicionInicial = Camera.main.transform.position;
-    }
+    //void Start()
+    //{
+    //    posicionInicial = Camera.main.transform.position;
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-        //transform.position = Vector3.Lerp(startMarker.positionInicial, endMarker.position.vector3, fracJourney);
-        transform.position = Vector3.Lerp(posicionInicial.position, Vector3.position, fracJourney);
-    }
+    //void Update()
+    //{
+    //    //transform.position = Vector3.Lerp(startMarker.positionInicial, endMarker.position.vector3, fracJourney);
+    //    transform.position = Vector3.Lerp(posicionInicial.position, Vector3.position, fracJourney);
+    //}
 
-    void OnMouseDown()
-    {
-        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-        Debug.Log("Click");
-    }
+    //void OnMouseDown()
+    //{
+    //    Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    //    Debug.Log("Click");
+    //}
 }
--- Assets/Pruebas/AnaMarchand/Scripts/movimientoEnemigo.cs
+++ Assets/Pruebas/AnaMarchand/Scripts/movimientoEnemigo.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movimientoEnemigo : MonoBehaviour
 {
-    public float moveTime = 0.1f; //
+    public float visionRadius = 20;
+    public float velocidad = 1;
 
-    // Start is called before the first frame update
+    GameObject player;
+
+    Vector3 posicionInicial;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        posicionInicial = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 target = posicionInicial;
+
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadius) target = player.transform.position;
+
+        float fixedSpeed = velocidad * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
     }
 }
