575a5a23b368e15442b6f82156dfe07875dfe8c8
Reyseta <luis.arg.gar@gmail.com>
2019-02-15 20:41:05 +0100

    ignorar

---
+++ Assets/Combate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combate : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+   
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(key))
+        }
+        
+    }
+}
