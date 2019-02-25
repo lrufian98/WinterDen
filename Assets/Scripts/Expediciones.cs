dd4f3181012d45179e23a81289b9a01bf70bbce2
CharlieC5 <carcasi_5@hotmail.com>
2019-02-25 20:18:19 +0100

    TP a habitacion de expediciones

---
+++ Assets/Scripts/Expediciones.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expediciones : MonoBehaviour
+{
+    public GameObject PosicionExpediciones;
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("HabitacionExpediciones"))
+        {
+            transform.position = PosicionExpediciones.transform.position;
+        }
+    }
+}
