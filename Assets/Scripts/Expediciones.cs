c8063478e3fcd86e11f277b5bec1675de813a5ae
CharlieC5 <carcasi_5@hotmail.com>
2019-02-25 20:57:27 +0100

    HabitacionDelOlvido
    Trabajo en las expediciones

--- Assets/Scripts/Expediciones.cs
+++ Assets/Scripts/Expediciones.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Expediciones : MonoBehaviour
 {
     public GameObject PosicionExpediciones;
+    
+    Transform puntoOrigen;
+
+    public void OnMouseDown()
+    {
+        puntoOrigen = gameObject.transform;
+        Debug.Log("Habitación Guardada");
+    }
     public void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("HabitacionExpediciones"))
@@ -12,4 +20,26 @@ public class Expediciones : MonoBehaviour
             transform.position = PosicionExpediciones.transform.position;
         }
     }
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("HabitacionDelOlvido"))
+        {
+            Debug.Log("Habitación Guardada");
+            Invoke("DeVuelta", Random.Range(2f, 3f));
+        }
+    }
+
+    void DeVuelta()
+    {
+        gameObject.transform.position = puntoOrigen.position;
+    }
+
+
+
+
+
+
+
+
+
 }
