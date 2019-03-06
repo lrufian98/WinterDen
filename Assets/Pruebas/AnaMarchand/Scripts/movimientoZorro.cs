30a5ed4ec64400822fe2fe45c218909b5e8be5a3
AnaMarchand <ana.marchand@hotmail.com>
2019-03-06 20:15:18 +0100

    #94 Mecánica de zorros y ascensores

--- Assets/Pruebas/AnaMarchand/Scripts/movimientoZorro.cs
+++ Assets/Pruebas/AnaMarchand/Scripts/movimientoZorro.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class movimientoZorro : MonoBehaviour
 {
     public float velocidad = 5f;
+    private bool derecha = true;
+
+    private int direccion;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direccion = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(velocidad * Time.deltaTime, 0, 0);
+
+        transform.Translate(velocidad * Time.deltaTime * direccion, 0, 0);
+    }
+
+    public void flip()
+    {
+        derecha = !derecha;
+        if (derecha)
+        {
+            direccion = 1;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            direccion = -1;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 }
