76f81b28631f30f4afb2d16c0f2505194e12757b
AnaMarchand <ana.marchand@hotmail.com>
2019-03-06 18:06:53 +0100

    Proceso Sonido

---
+++ Assets/Pruebas/AnaMarchand/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip pasos;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pasos = Resources.Load<AudioClip>("Conejos", "Zorros");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string clip)
+    {
+
+    }
+}
